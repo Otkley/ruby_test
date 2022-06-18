1fd9e9a093da32350c3738b62489e04afef9c098
Sergio Montiel <sergiomontielcastello@gmail.com>
2022-06-18 10:59:36 -0500

    Finish test

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,1 +1,16 @@
 # Escribe tu código del ejercicio en este archivo. Haz que las pruebas en test/dictionary_test.rb pasen
+class Dictionary
+  attr_accessor :word
+
+  def initialize(words)
+    @words = words
+  end
+
+  def find_meaning(w)
+    @words.each do |word|
+      if word.word == w
+        return word.meaning
+      end
+    end
+  end
+end
